15ebbcd921164c75c34a13d2f6bb9d05f9a425bf
github username <github email address>
2019-07-08 15:59:49 -0700

    Added create function and it works

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,27 @@
 class UsersController < ApplicationController
-  before_action :user_params only: [:new]
+  before_action :user_params, only: [:create]
+
   def index
     @users = User.all
   end
 
   def new
+    @user = User.new
+  end
+
+  def create
     @user = User.new(user_params)
-    @user.save
+    if @user.save
+      redirect_to user_path(@user)
+    else
+      render :new
+    end
   end
 
-  private
 
-  def user_params
+ private
+
+   def user_params
     params.require(:user).permit(:username, :password, :name, :certification, :age, :country, :total_dives)
-  end
+   end
 end
