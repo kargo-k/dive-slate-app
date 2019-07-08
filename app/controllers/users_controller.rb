44cbd198656da06a243d9d8b512c870d69427f28
github username <github email address>
2019-07-08 16:34:21 -0700

    user create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,20 @@
 class UsersController < ApplicationController
-  before_action :user_params only: [:new]
+  before_action :user_params only: [:create]
   def index
     @users = User.all
   end
 
   def new
+    @user = User.new
+  end
+
+  def create
     @user = User.new(user_params)
-    @user.save
+    if @user.save
+      redirect_to user_path(@user)
+    else
+      render :new
+    end
   end
 
   private
