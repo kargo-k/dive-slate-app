9266568032160916fe028c7bf194a2b326dcabf3
github username <github email address>
2019-07-08 15:15:11 -0700

    Did user view/index and methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,17 @@
 class UsersController < ApplicationController
+  before_action :user_params only: [:new]
+  def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new(user_params)
+    @user.save
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password, :name, :certification, :age, :country, :total_dives)
+  end
 end
