0b5e139f775a126d194765f497defd4c5768b1e4
Karen Go <karenrgo@gmail.com>
2019-07-08 17:01:01 -0700

    index view for divesites

--- app/controllers/diveshops_controller.rb
+++ app/controllers/diveshops_controller.rb
@@ -2,4 +2,8 @@ class DiveshopsController < ApplicationController
     def show
         @diveshop = Diveshop.find(params[:id])
     end
+
+    def index
+        @diveshops = Diveshop.all 
+    end
 end
--- app/controllers/divesites_controller.rb
+++ app/controllers/divesites_controller.rb
@@ -2,4 +2,8 @@ class DivesitesController < ApplicationController
     def show
         @divesite = Divesite.find(params[:id])
     end
+
+    def index
+        @divesites = Divesite.all
+    end
 end
