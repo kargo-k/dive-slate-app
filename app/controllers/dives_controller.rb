353f4c41ceb5b8f77add1fc67230a816753a10d7
Karen Go <karenrgo@gmail.com>
2019-07-08 18:00:17 -0700

    added some dives functionality

--- app/controllers/dives_controller.rb
+++ app/controllers/dives_controller.rb
@@ -1,2 +1,21 @@
 class DivesController < ApplicationController
+    def new
+        @dive = Dive.new
+    end
+
+    def create
+        @dive = Dive.new(dive_params)
+        if @dive
+            redirect_to @dive
+        else
+            render :new
+        end
+    end
+
+    private
+
+        def dive_params
+            params.require(:dive).permit(:user_id, :diveshop_id, :divesite_id, :date, :time, :water_T, :air_T, :depth, :description)
+        end
+
 end
