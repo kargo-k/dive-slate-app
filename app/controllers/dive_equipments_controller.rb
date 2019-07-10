d506b80f9974282cfdde7507b2cca7fcdd177d0f
github username <github email address>
2019-07-10 10:29:34 -0700

    added dive & equipment function

--- app/controllers/dive_equipments_controller.rb
+++ app/controllers/dive_equipments_controller.rb
@@ -1,3 +1,13 @@
 class DiveEquipmentsController < ApplicationController
- 
+
+
+    def new
+    end
+
+    def create
+      @diveequipment = DiveEquipment.new(params.require(:diveequipment).permit(:dive_id, :equipment_id))
+      @diveequipment.save
+
+      redirect_to dives_show_path
+    end
 end
